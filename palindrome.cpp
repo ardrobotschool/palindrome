5b34e8eb5eea0160912780af221ebdbd55e4a056
Artur <405502@bsd48.org>
2016-09-18 16:48:59 -0700

    first legit commit

---
+++ palindrome.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+#include <ctype.h>
+
+using namespace std;
+
+int main(){
+  cout << "Enter your string: ";
+  char str[81];
+  cin.getline(str, 81);
+  char processedStr[81];
+
+  //Must remove all punctuation.
+  
+  return 0;
+}
